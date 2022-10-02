b29e5f8ec54b6a0f7cf7750124541ebb2ea68f51
Abdelrahman Ghazaly <abdelrahman.ghazaly2101@gmail.com>
2022-10-02 17:23:22 +0200

    prototype_flutter

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prototype_flutter/constants/dummy_data.dart';
 import 'package:prototype_flutter/models/product.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
@@ -63,21 +64,113 @@ class ProductScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(product.name),
+                  Text(
+                    product.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    product.category,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: screenHeight * 0.5,
+                width: screenWidth,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(product.image),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    product.price.toString(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
                   RatingBar(
                     ratingWidget: ratingWidget,
                     onRatingUpdate: (_) {},
                     initialRating: 3.5,
                     allowHalfRating: true,
                     ignoreGestures: true,
-                    itemSize: 20,
-                  )
+                    itemSize: 30,
+                  ),
                 ],
               ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Desription',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(product.description),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Recommend for You',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Container(
+                height: screenWidth * 0.5,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: kDummyProducts.length,
+                  itemBuilder: (_, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: screenHeight * 0.2,
+                            width: screenWidth * 0.5,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.scaleDown,
+                                image: NetworkImage(
+                                  kDummyProducts[index].image,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(kDummyProducts[index].name),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(kDummyProducts[index].price.toString()),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
             ],
           ),
         ),
